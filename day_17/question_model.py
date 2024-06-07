425e73f03032e0211c6083c91f2a9cca8e38dc29
RAVI MANISANKAR REDDY <137458271+Manisank@users.noreply.github.com>
2024-06-07 10:40:43 +0530

    Add files via upload

---
+++ day_17/question_model.py
@@ -0,0 +1,11 @@
+class Question:
+    def __init__(self ,  q_text , q_answer):
+        self.text = q_text
+        self.answer = q_answer
+
+user = Question("Mani",'True')
+
+#print(user.text)
+
+
+
